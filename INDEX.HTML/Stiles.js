c54c22b1682c9811132cf4e95eab063dd8c399cb
JACKECRUZ <91508146+JACKECRUZ@users.noreply.github.com>
2022-03-24 21:09:22 -0500

    Add files via upload

---
+++ INDEX.HTML/Stiles.js
@@ -0,0 +1,29 @@
+
+        <fieldset>
+        <div><select class="tipos" id="tipo">Tipo de computadora</select></div>
+
+        <label >Cantidad de computadoras</label>
+        <input type="number" id="cantidad"/><br>
+
+        <button id="boton" type="submit"> Calcular Compra  </button>
+
+    </fieldset>
+</form>
+
+<form>
+    <fieldset>
+        <h2>Orden de compra</h2>
+            
+
+
+        <label >Valor de compra</label>
+        <input id="vCompra" type="number"/><br>
+
+        <label >Valor de descuento</label>
+        <input id="vDescuento"  type="number"/><br>
+
+        <label >Valor total con descuento</label>
+        <input id="vTotal"  type="number"/><br>
+
+        <label id="descuento"></label>
+<form>
